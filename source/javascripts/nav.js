febea5f646bc3183aa65df2d1e7120122e400d8a
Adam <adamgoodapp@gmail.com>
2013-11-21 18:00:10 +0000

    Adds second nav

---
+++ source/javascripts/nav.js
@@ -0,0 +1,29 @@
+//toggle menu view   
+$(function() {
+    if ($(window).width() > 480) {
+        $('nav strong').css('display','none');
+        $('nav ul').show();
+    }
+    else {
+        $('nav strong').css('display','block');
+        $('nav ul').hide();
+    }
+});
+// check for window resize - show nav again for larger screens after hiding
+$(window).resize(function() {
+    if ($(window).width() > 480) {
+        $('nav strong').css('display','none');
+        $('nav ul').show();
+    }
+    else {
+        $('nav strong').css('display','block');
+        $('nav ul').hide();
+    }
+});
+
+// show menu (smallest screens)
+$("nav strong").click(function(e){
+    e.preventDefault();
+    $("nav ul").slideToggle("fast");   
+      return false;
+});
